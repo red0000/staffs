ee060ed9433afacb459d6321cde9491c91e0d030
bsg <challenger3@ngs.ru>
2019-11-21 13:51:12 +0700

    Добавьте файлы проекта.

---
+++ Staffs/Data/StaffsContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Staffs.Models
+{
+    public class StaffsContext : DbContext
+    {
+        public StaffsContext (DbContextOptions<StaffsContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Staffs.Models.TodoItem> TodoItem { get; set; }
+    }
+}
---
+++ Staffs/Models/TodoItem.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Staffs.Models
+{
+    public class TodoItem
+    {
+            public long Id { get; set; }
+            public string first_name { get; set; }
+            public string middle_name { get; set; }
+            public string last_name { get; set; }
+            public DateTime birth_date { get; set; }
+            public DateTime bdate { get; set; }
+            public DateTime edate { get; set; }
+            public bool is_active { get; set; }
+            public DateTime create_date { get; set; }
+    }
+}
